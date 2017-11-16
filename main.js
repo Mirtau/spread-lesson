e1703a3d17e153916c720e8f54269b32876599a2
Mirtau <tjj72@comcast.net>
2017-11-16 11:17:35 -0700

    complete

--- main.js
+++ main.js
@@ -5,7 +5,8 @@
 //use rest in function parameter
 //if input is (1, 2, 3) the function should return [2, 4, 6]
 //the function should account for n number of arguments
-const rest = (input) => input.map(element => element * 2);
+const rest = (...input) => input.map(element => element * 2);
+
 
 // function rest(input) {
 //   return input.map(function(element) {
@@ -17,7 +18,7 @@ const rest = (input) => input.map(element => element * 2);
 
 //use spread to concat arrays
 //if first and last are ([1, 2, 3], [4, 5, 6]) return [1, 2, 3, 4, 5, 6]
-const concat = (first, last) => first.concat(last);
+const concat = (first, last) => [...first, ...last];
 
 // function concat(first, last) {
 //   return first.concat(last);
@@ -28,7 +29,7 @@ const concat = (first, last) => first.concat(last);
 //use spread to spread an array
 //if input is [1, 2, 3] return 6
 const add = (a, b, c) => a + b + c;
-const spreadAdd = input => add(input);
+const spreadAdd = input => add(...input);
 
 // function add(a, b, c) {
 //   return a + b + c;
@@ -41,7 +42,7 @@ const spreadAdd = input => add(input);
 
 //use spread with Math.max/min
 //if input is [1, 2, 4, 7, 9] return 9
-const mathSpread = numbers => Math.max(numbers);
+const mathSpread = numbers => Math.max(...numbers);
 
 //circumvents the use of apply
 
